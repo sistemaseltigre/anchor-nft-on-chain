aae91fcda8020d974d8bd7a55dcd77b81dff2913
Jesus Silva <sistemaseltigre@gmail.com>
2023-08-16 20:00:12 -0400

    create collection nft on chain

--- lib.rs
+++ lib.rs
@@ -101,8 +101,105 @@ pub mod nft_program {
         Ok(())
     }
 
-}
+    pub fn mint_to_collection(
+        ctx: Context<MintToCollection>,
+        id_collection: u64,
+        id_nft: u64,
+        name: String,
+        symbol: String,
+        uri: String,
+        price: f32,
+        cant: u64,
+    ) -> Result<()> {
+        msg!("Creating seeds");
+        let id_bytes = id_collection.to_le_bytes();
+        let id_nft_bytes = id_nft.to_le_bytes(),
+        let seeds = &[
+            "mint".as_bytes(),
+            id_bytes.as_ref(),
+            id_nft_bytes.as_ref(),
+            &[*ctx.bumps.get("mint").unwrap()],
+        ];
 
+        msg!("Run mint_to");
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.authority.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &[&seeds[..]],
+            ),
+            1, // 1 token
+        )?;
+
+        msg!("Run create metadata accounts v3");
+
+        create_metadata_accounts_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    payer: ctx.accounts.payer.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                    metadata: ctx.accounts.nft_metadata.to_account_info(),
+                    mint_authority: ctx.accounts.authority.to_account_info(),
+                    update_authority: ctx.accounts.authority.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &[&seeds[..]],
+            ),
+            DataV2 {
+                name,
+                symbol,
+                uri,
+                seller_fee_basis_points: 0,
+                creators: Some(vec![Creator {
+                    address: ctx.accounts.payer.key(),
+                    verified: true,
+                    share: 100,
+                }]),
+                collection: Some(Collection {
+                    key: ctx.accounts.collection.key(),
+                    verified: false,
+                }),
+                uses: None,
+            },
+            true,
+            true,
+            None,
+        )?;
+
+        msg!("Run create master edition v3");
+
+        create_master_edition_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    edition: ctx.accounts.master_edition_account.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                    metadata: ctx.accounts.nft_metadata.to_account_info(),
+                    mint_authority: ctx.accounts.authority.to_account_info(),
+                    update_authority: ctx.accounts.authority.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &[&seeds[..]],
+            ),
+            Some(1),
+        )?;
+
+        msg!("Minted NFT successfully");
+
+        Ok(())
+    }
+
+}
 
 #[derive(Accounts)]
 #[instruction(id: u64)]
@@ -158,4 +255,64 @@ pub struct CreateNFT<'info> {
     )]
     /// CHECK:
     pub nft_metadata: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(id_collection: u64, id_nft: u64)]
+pub struct MintToCollection<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account( 
+    init,
+    payer = payer, 
+    mint::decimals = 0,
+    mint::authority = authority,
+    mint::freeze_authority = authority,
+    seeds = ["mint".as_bytes(), 
+             id_collection.to_le_bytes().as_ref(),
+             id_nft.to_le_bytes().as_ref()], 
+    bump,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub metadata_program: Program<'info, Metadata>,
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            mint.key().as_ref(),
+            b"edition".as_ref(),
+        ],
+        bump,
+        seeds::program = metadata_program.key()
+    )]
+    /// CHECK:
+    pub master_edition_account: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            mint.key().as_ref(),
+        ],
+        bump,
+        seeds::program = metadata_program.key()
+    )]
+    /// CHECK:
+    pub nft_metadata: UncheckedAccount<'info>,
+    /// CHECK:
+    pub collection: UncheckedAccount<'info>,
 }
